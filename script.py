dec73fa7739dcde290dff892cc799f31ccb997cb
Rahmat Ramadhani <47129927+rramadhani015@users.noreply.github.com>
2025-02-18 16:14:51 +0700

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,12 @@
 import requests
 
 def hit_api():
-    url = "https://api.example.com/data"  # Change to your API
-    #headers = {"Authorization": "Bearer YOUR_API_KEY"}  # Add if needed
+    url = "http://overpass-api.de/api/interpreter?data=[out:json][timeout:25];area['name'='Daerah Khusus Ibukota Jakarta']->.a;(node['amenity'='pharmacy'](area.a);way['amenity'='pharmacy'](area.a);relation['amenity'='pharmacy'](area.a););out center tags;"
+    
     response = requests.get(url)
 
     if response.status_code == 200:
-        print("API Response:", response.json())
+        print("API Response:", response.json())  # Adjust this if you need specific data
     else:
         print("Error:", response.status_code, response.text)
 
